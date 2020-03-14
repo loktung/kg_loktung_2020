6fd92c9136d0c99bdbbf8074c7c67d045990ffe7
loktung <47838124+loktung@users.noreply.github.com>
2020-03-14 12:37:16 -0400

    Update main.py
    Map string with more than 3 characters.

--- main.py
+++ main.py
@@ -3,27 +3,27 @@
 #^o already map to a cannot map to another character
 #CASE3: s1=bar s2=foo print True b->f a->o r->o
 #^a map to o and r map to o (Is not like o map to a and r)
-#Only for 3 characters string
 
 #!/usr/bin/env 
 from sys import argv
 #pass argument
 
 def split(s1):
-#Split s1 into characters
+#Split s1 into elements
     charlist=list(s1)
-    firstchar=charlist[0]
-#The [0] as the first character
-    x=1
-#x=1 charlist[1] is the second character
-    while x < len(charlist):
-        if firstchar != charlist[x]:
-            #if the first and second character does not match
-            x=x+1#move to the next character
-            #do nothing
-        else:
-            return False
-    if charlist[1]!=charlist[2]: #compare the second and the third char
+    x=0
+    y=1
+#charlist[x] first element
+    for x in range (0,(len(charlist)-1)):#go through each elements
+        
+        while y < len(charlist):#compare the first element with the rest  
+            if charlist[x] != charlist[y]:
+                #if the first and second character does not match
+                y=y+1#move to the next character
+                #do nothing
+            else:
+                return False
+    if charlist[len(charlist)-1]!=charlist[len(charlist)-2]: #compare [-1]&[-2]
         return True
     else:
         return False
